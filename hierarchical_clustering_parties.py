3df4634bf77e3152158bbcf1eb5465db1e47bd81
Marcus-Friis <marcus.friis99@gmail.com>
2022-12-28 13:14:20 +0100

    More plots <3

--- hierarchical_clustering_parties.py
+++ hierarchical_clustering_parties.py
@@ -1,5 +1,4 @@
 import matplotlib as mpl
-from matplotlib.gridspec import GridSpec
 from matplotlib.rcsetup import cycler
 from sklearn.cluster import AgglomerativeClustering
 import numpy as np
@@ -8,7 +7,6 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.cluster.hierarchy import dendrogram
-from scipy import stats
 
 
 df = pd.read_csv('data/kandidater_data.csv').dropna()
@@ -20,16 +18,8 @@ agg = df.groupby('CurrentPartyCode').agg(
 
 # clusterer = AgglomerativeClustering(n_clusters=None, distance_threshold=24)
 clusterer = AgglomerativeClustering(n_clusters=14, compute_distances=True)
-clusters = clusterer.fit_predict(df[[str(i) for i in range(1, 26)]])
-df['cluster'] = clusters
-
-# Apply t-SNE to the selected columns
-tsne = TSNE(n_components=2, random_state=69420)
-X_tsne = tsne.fit_transform(df[[str(i) for i in range(1, 26)]])
-
-# Add the transformed coordinates to the existing dataframe
-df['tsne1'] = X_tsne[:, 0]
-df['tsne2'] = X_tsne[:, 1]
+clusters = clusterer.fit_predict(agg[[str(i) for i in range(1, 26)]])
+agg['cluster'] = clusters
 
 colormap = {'A': '#A82721', 
             'O': '#EAC73E', 
@@ -37,52 +27,17 @@ colormap = {'A': '#A82721',
             'Ø': '#E6801A', 
             'I': '#3FB2BE', 
             'Å': '#2B8738', 
-            'B': '#522170',   # '#733280', 
+            'B': '#733280', 
             'F': '#E07EA8', 
             'C': '#96B226', 
             'D': '#127B7F', 
             'K': '#8B8474', 
             'Q': '#C49414', 
-            'M': '#943CA4',  # '#832B93' 
+            'M': '#832B93', 
             'Æ': '#2C5877'}
 df['PartyColor'] = df.CurrentPartyCode.map(colormap)
 
 plt.style.use('ggplot')
-
-fig = plt.figure(figsize=(15,7))
-gs = GridSpec(2, 2, figure=fig)
-
-ax = fig.add_subplot(gs[0, 0])
-ax.scatter(df.tsne1, df.tsne2, color=df.PartyColor, label=df.CurrentPartyCode, alpha=.6)
-ax.set_title('True parties dim-reduced with t-sne')
-ax.set_xlabel('t-sne 1')
-ax.set_ylabel('t-sne 2')
-
-
-ax = fig.add_subplot(gs[1, 0])
-# color_palette = sns.color_palette('Paired', 14)
-color_palette = {
-    0: 'V',
-    1: 'Ø',
-    2: 'K',
-    3: 'M',
-    4: 'Q',  # Nyt parti, blevet til Q
-    5: 'B',
-    6: 'D',
-    7: 'O',
-    8: 'C',
-    9: 'Å',
-    10: 'Æ',
-    11: 'F',
-    12: 'I',
-    13: 'A'
-}
-color = [colormap[color_palette[i]] for i in df.cluster]
-
-ax.scatter(df.tsne1, df.tsne2, color=color, alpha=.6)
-ax.set_title('Clusters dim-reduced with t-sne')
-ax.set_xlabel('t-sne 1')
-ax.set_ylabel('t-sne 2')
 
 def plot_dendrogram(model, **kwargs):
     # Create linkage matrix and then plot the dendrogram
@@ -106,27 +61,38 @@ def plot_dendrogram(model, **kwargs):
     # Plot the corresponding dendrogram
     dendrogram(linkage_matrix, **kwargs)
 
+
+# def llf(id):
+#     if id < 14:
+#         return agg.CurrentPartyCode.iloc[id]
+#     else:
+        # return str(id)
+
 def llf(id):
     if id < df.shape[0]:
-        return df.CurrentPartyCode.iloc[id]
+        return agg.CurrentPartyCode.iloc[id]
     else:
         return str(id)
 
+
+
 mpl.rcParams['axes.prop_cycle'] = cycler('color', ['#988ED5',  '#348ABD', '#E24A33', '#777777', '#FBC15E', '#8EBA42', '#FFB5B8'])
 
-ax = fig.add_subplot(gs[:, 1])
+fig, ax = plt.subplots()
+agg = df.groupby('CurrentPartyCode').agg({f'{i}': 'mean' for i in range(1, 26)}).reset_index()
+
+# clusterer = AgglomerativeClustering(n_clusters=None, distance_threshold=24)
+# clusters = clusterer.fit_predict(agg[[str(i) for i in range(1, 26)]])
+
 params = {'ax': ax, 'leaf_label_func': llf, 'leaf_rotation': 0, 'show_leaf_counts': True}
 plot_dendrogram(clusterer, truncate_mode="level", **params)
 ax.grid(False)
 ax.set_yticks([])
 ax.set_title('Hierarchical clustering of party means')
-ax.set_xlabel('Candidates')
+ax.set_xlabel('Parties')
 
 xlbls = ax.get_xmajorticklabels()
 for lbl in xlbls:
     lbl.set_color(colormap[lbl.get_text()])
 
-ax.set_xticks([])
-
-plt.tight_layout()
-plt.savefig('tsne_dendogram.svg')
+plt.savefig('dendogram_parties.svg')
